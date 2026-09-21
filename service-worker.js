6d55ce7425a34b5536b3f1e07be557e086c4d329
ts-serviceleistungen <Info@ts-serviceleistungen.de>
2026-09-21 13:10:56 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "ts-kalender-v14";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./style.css?v=14",
+  "./app.js",
+  "./config.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+  event.respondWith(
+    fetch(req).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(() => {});
+      return response;
+    }).catch(() => caches.match(req).then(r => r || caches.match("./index.html")))
+  );
+});
